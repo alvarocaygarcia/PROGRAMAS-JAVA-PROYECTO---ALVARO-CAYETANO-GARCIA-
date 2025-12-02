21ccdb0d3e4a0d704b3144dbd4321cbbee0bf63b
Álvaro Cayetano García <alvarogarciacay@gmail.com>
2025-12-02 19:14:11 +0100

    Add files via upload

---
+++ Ejercicio 8 Java - Alvaro Cayetano.java
@@ -0,0 +1,54 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
+        // to see how IntelliJ IDEA suggests fixing it.
+        System.out.printf("Hello and welcome!");
+
+        for (int i = 1; i <= 5; i++) {
+            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
+            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
+            System.out.println("i = " + i);
+
+
+            //Ejercicio 8
+
+            //A
+            boolean vacia=false;
+            boolean llena=true;
+
+            //B
+            int edad=45;
+
+            //C
+            float factura=120.40F;
+
+            //D
+            char soltero='s';
+            char casado='c';
+            char viudo='v';
+            char divorciado='d';
+            System.out.println("Su estado civil es: "+soltero);
+
+            //E
+            String a="programación";
+
+            //F
+            double areaFigura=234.30;
+            System.out.println("El area de la figura es: "+areaFigura);
+
+            //G
+            boolean aprobado=true;
+            boolean suspenso=false;
+
+
+
+
+
+
+
+
+        }
+    }
+}
